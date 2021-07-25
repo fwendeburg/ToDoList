630f8ac4b96883330ee19fc974a3d0360c67c908
fwendeburg <86864927+fwendeburg@users.noreply.github.com>
2021-07-25 03:10:28 +0200

    webpack final config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        DOMman: './src/DOMmanipulation.js',
+        animations: './src/animations.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: './src/index.html',
+            inject: 'body'
+        }),
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
 };
