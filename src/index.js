a816c5a93c0f73e319dd4c1038cc5e9132db548f
Francisco Wendeburg <wendeburgf@gmail.com>
2022-08-15 06:01:06 +0200

    webpack now bundles css files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import UI from './UI';
 import App from './App';
 
+import './styles/reset.css';
+import './styles/style.css';
+
 App.initApp();
 UI.initUI();
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    module: {
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+       ]
+    },
     entry: {
         index: './src/index.js',
         Storage: './src/Storage.js',
